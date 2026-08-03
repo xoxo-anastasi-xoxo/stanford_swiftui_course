535d5931f11473004003c1ba6c007fc15a6754d3
Anastasiia Kazantseva <xoxo@Anastasiias-MacBook-Pro.local>
2026-08-03 10:51:06 +0100

    [2025] Initial commit - setting up 2025 cource version

---
+++ 2025/CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Anastasiia Kazantseva on 03/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
